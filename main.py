d3eb00fd1e6ba0abe5fb147fa25b46b86082f248
shmuelshwartz <shmuelshwartzcode@gmail.com>
2025-07-02 08:32:50 +0100

    Add timer logic to Rest screen

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+from kivy.properties import StringProperty, NumericProperty
+from kivy.clock import Clock
+import time
+
+
+class RestScreen(MDScreen):
+    timer_label = StringProperty("20")
+    target_time = NumericProperty(0)
+
+    def on_enter(self, *args):
+        self.target_time = time.time() + 20
+        self.update_timer(0)
+        self._event = Clock.schedule_interval(self.update_timer, 1)
+        return super().on_enter(*args)
+
+    def on_leave(self, *args):
+        if hasattr(self, "_event") and self._event:
+            self._event.cancel()
+        return super().on_leave(*args)
+
+    def update_timer(self, dt):
+        remaining = int(self.target_time - time.time())
+        if remaining <= 0:
+            self.timer_label = "0"
+            if hasattr(self, "_event") and self._event:
+                self._event.cancel()
+            if self.manager:
+                self.manager.current = "workout_active"
+        else:
+            self.timer_label = str(remaining)
+
+    def adjust_timer(self, seconds):
+        self.target_time += seconds
+        if self.target_time - time.time() <= 0:
+            if hasattr(self, "_event") and self._event:
+                self._event.cancel()
+            if self.manager:
+                self.manager.current = "workout_active"
+
+
 
 
 class WorkoutApp(MDApp):
