7cd48a00a785a5322da696573e86039250325a06
shmuelshwartz <shmuelshwartzcode@gmail.com>
2025-07-16 17:15:34 +0100

    Allow spaces in enum values input

--- main.py
+++ main.py
@@ -1064,7 +1064,7 @@ class AddMetricPopup(MDDialog):
             elif input_type == "float":
                 allowed = string.digits + ".,"
             else:  # default to str
-                allowed = string.ascii_letters + ","
+                allowed = string.ascii_letters + " ,"
 
             def _filter(value, from_undo):
                 return "".join(ch for ch in value if ch in allowed)
@@ -1317,7 +1317,7 @@ class EditMetricPopup(MDDialog):
             elif input_type == "float":
                 allowed = string.digits + ".,"
             else:
-                allowed = string.ascii_letters + ","
+                allowed = string.ascii_letters + " ,"
 
             def _filter(value, from_undo):
                 return "".join(ch for ch in value if ch in allowed)
