dcc468fc76a90ffb9e4fe839522e35627f7260d1
shmuelshwartz <shmuelshwartzcode@gmail.com>
2025-07-29 09:22:38 +0100

    updated db

---
+++ data/workout_schema.sql
@@ -0,0 +1,124 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "library_exercise_metrics" (
+	"id"	INTEGER,
+	"exercise_id"	INTEGER NOT NULL,
+	"metric_type_id"	INTEGER NOT NULL,
+	"position"	INTEGER DEFAULT 0,
+	"input_type"	TEXT,
+	"source_type"	TEXT,
+	"input_timing"	TEXT,
+	"is_required"	BOOLEAN,
+	"scope"	TEXT CHECK("scope" IS NULL OR "scope" IN ('preset', 'session', 'section', 'exercise', 'set')),
+	"enum_values_json"	TEXT,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("exercise_id") REFERENCES "library_exercises"("id") ON DELETE CASCADE,
+	FOREIGN KEY("metric_type_id") REFERENCES "library_metric_types"("id") ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "library_exercises" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	"description"	TEXT,
+	"is_user_created"	BOOLEAN NOT NULL DEFAULT 0,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "library_metric_types" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	"input_type"	TEXT NOT NULL CHECK("input_type" IN ('int', 'float', 'str', 'bool')),
+	"source_type"	TEXT NOT NULL CHECK("source_type" IN ('manual_text', 'manual_enum', 'manual_slider')),
+	"input_timing"	TEXT NOT NULL CHECK("input_timing" IN ('library', 'preset', 'pre_workout', 'post_workout', 'pre_set', 'post_set')),
+	"is_required"	BOOLEAN DEFAULT FALSE,
+	"scope"	TEXT NOT NULL CHECK("scope" IN ('preset', 'session', 'section', 'exercise', 'set')),
+	"description"	TEXT,
+	"is_user_created"	BOOLEAN NOT NULL DEFAULT 0,
+	"enum_values_json"	TEXT,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "preset_exercise_metrics" (
+	"id"	INTEGER,
+	"section_exercise_id"	INTEGER NOT NULL,
+	"metric_name"	TEXT NOT NULL,
+	"metric_type_id"	INTEGER NOT NULL,
+	"input_type"	TEXT NOT NULL,
+	"source_type"	TEXT NOT NULL,
+	"input_timing"	TEXT NOT NULL,
+	"is_required"	BOOLEAN NOT NULL DEFAULT 0,
+	"scope"	TEXT NOT NULL CHECK("scope" IN ('exercise', 'set')),
+	"position"	INTEGER NOT NULL DEFAULT 0,
+	"library_metric_type_id"	INTEGER,
+	"enum_values_json"	TEXT,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("metric_type_id") REFERENCES "library_metric_types"("id") ON DELETE NO ACTION,
+	FOREIGN KEY("section_exercise_id") REFERENCES "preset_section_exercises"("id") ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "preset_preset_metrics" (
+	"id"	INTEGER,
+	"preset_id"	INTEGER NOT NULL,
+	"metric_type_id"	INTEGER NOT NULL,
+	"value"	TEXT,
+	"position"	INTEGER DEFAULT 0,
+	"input_type"	TEXT NOT NULL CHECK("input_type" IN ('int', 'float', 'str', 'bool')),
+	"source_type"	TEXT NOT NULL CHECK("source_type" IN ('manual_text', 'manual_enum', 'manual_slider')),
+	"input_timing"	TEXT NOT NULL CHECK("input_timing" IN ('library', 'preset', 'pre_workout', 'post_workout')),
+	"is_required"	BOOLEAN DEFAULT 0,
+	"scope"	TEXT NOT NULL CHECK("scope" = 'preset'),
+	"enum_values_json"	TEXT,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("metric_type_id") REFERENCES "library_metric_types"("id") ON DELETE NO ACTION,
+	FOREIGN KEY("preset_id") REFERENCES "preset_presets"("id") ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "preset_presets" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "preset_section_exercises" (
+	"id"	INTEGER,
+	"section_id"	INTEGER NOT NULL,
+	"exercise_name"	TEXT NOT NULL,
+	"exercise_description"	TEXT,
+	"position"	INTEGER NOT NULL,
+	"number_of_sets"	INTEGER NOT NULL DEFAULT 1,
+	"library_exercise_id"	INTEGER,
+	"rest_time"	INTEGER NOT NULL DEFAULT 120,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("section_id") REFERENCES "preset_sections"("id") ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "preset_sections" (
+	"id"	INTEGER,
+	"preset_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"position"	INTEGER NOT NULL,
+	"deleted"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("preset_id") REFERENCES "preset_presets"("id") ON DELETE CASCADE
+);
+CREATE VIEW library_view_exercise_metrics AS
+    SELECT
+        em.id AS exercise_metric_id,
+        em.exercise_id,
+        e.name AS exercise_name,
+        em.metric_type_id,
+        mt.name AS metric_type_name
+    FROM
+        library_exercise_metrics em
+    JOIN
+        library_exercises e ON em.exercise_id = e.id
+    JOIN
+        library_metric_types mt ON em.metric_type_id = mt.id;
+CREATE UNIQUE INDEX IF NOT EXISTS "idx_library_exercises_name_user_created" ON "library_exercises" (
+	"name",
+	"is_user_created"
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "idx_library_metric_types_name_user_created" ON "library_metric_types" (
+	"name",
+	"is_user_created"
+);
+COMMIT;
